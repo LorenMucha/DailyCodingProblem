919fa4ba2f97ba1d67625eb94e1fd622bde6235f
LorenMucha <lorenmucha@gmail.com>
2022-05-29 21:14:05 +0200

    add a bit refactoring

--- lib/problem_one.rb
+++ lib/problem_one.rb
@@ -10,16 +10,16 @@
 # Class to calculate solution
 class IntervalCalculator
   def self.claculate(input)
-    x = 0
-    count = 0
+    @x = 0
+    @count = 0
     input.each do |el|
       last = el.last
-      next unless x < input.length - 1
+      next unless @x < input.length - 1
 
-      first = input[x + 1].first
-      count += 1 if last >= first
-      x += 1
+      first = input[@x + 1].first
+      @count += 1 if last >= first
+      @x += 1
     end
-    count
+    @count
   end
 end
